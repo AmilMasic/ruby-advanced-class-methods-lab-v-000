c90fc9f952670647d286c88030dda5c1dc2c9992
Amil Masic <amil.masic@gmail.com>
2018-09-23 23:18:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,18 +44,18 @@ class Song
   #     end
   #   end
   def self.find_or_create_by_name(find_this_song)
-    # This method will accept a string name for a song and 
-    # either return a matching song instance with that name 
+    # This method will accept a string name for a song and
+    # either return a matching song instance with that name
     # or create a new song with the name and return the song instance.
     did_i_find_it = self.all.detect {|x| x.name == find_this_song}
     if did_i_find_it == nil
-      s = self.new 
-      s.name = find_this_song
-      s.save 
-      s
+      song.create_by_name
+      # s = self.new
+      # s.name = find_this_song
+      # s.save
+      song
     else
       did_i_find_it
     end
-  end 
-  # end
+  end
 end
