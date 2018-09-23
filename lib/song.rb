8922af872f6cbe810ca7b8c70b7030cebd6a851d
Amil Masic <amil.masic@gmail.com>
2018-09-23 22:57:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,4 +30,10 @@ class Song
     song.name = song_string_name
     song
   end
+
+  def self.create_by_name(song_string_name)
+    song = self.new
+    song.name = song_string_name
+    song
+    @@all << song
 end
