468db08122c6ec3bdd5a0e90369e3543a9c628c5
Amil Masic <amil.masic@gmail.com>
2018-09-23 22:48:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,17 @@ class Song
     self.class.all << self
   end
 
+  # def self.create
+  #   song = self.new
+  #   # song.name = name
+  #   song.save
+  # end
+  def initialize(name)
+    @name = name
+    # @@all << self
+  end
+  def self.create(name)
+    song = self.new
+    song.save
+  end
 end
